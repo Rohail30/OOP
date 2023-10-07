901bae67eff3d013cbc4f1f278601037a93ac6f2
Rohail30 <96627590+Rohail30@users.noreply.github.com>
2023-10-07 17:45:15 +0500

    Add files via upload

---
+++ OOP LAB/LAB-7/src/Extra/NewStudent.java
@@ -0,0 +1,9 @@
+package Extra;
+
+import com.company.Student;
+
+public class NewStudent extends Student {
+    public NewStudent(String name, String address) {
+        super(name, address);
+    }
+}
